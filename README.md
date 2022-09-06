21048d40f701fb94eef2fffc18576f8568b847ce
Valentin <valtron.forever@gmail.com>
2022-09-06 23:09:28 +0300

    First version

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # xl_parse
 Parse xlsx file with specific data format to json
+
+## Instalation
+Open project directory and prepare python virtual env
+```
+# Ubuntu Linux
+sudo apt install python3-venv
+python3 -m venv .venv
+source .venv/bin/activate
+
+# MacOS
+pip install virtualenv
+virtualenv .venv
+source .venv/bin/activate
+```
+
+Install project dependencies:
+```
+pip install -r requirements.txt
+```
+
+## Using
+Activate virtual environment (if is not active already):
+```
+source .venv/bin/activate
+```
+
+Run python script with file for parsing as param:
+```
+python parse.py input.xlsx
+```
+Result json will be saved to `output.json` file.
+Run `python parse.py --help` for additional usage info.
---
+++ parse.py
@@ -0,0 +1,97 @@
+from typing import TypedDict
+
+import json
+from pathlib import Path
+
+import pandas as pd
+from tqdm import tqdm as progress_bar
+import click
+ 
+
+class MetaDoesNotExistException(Exception):
+    pass
+
+
+class MetaInfo(TypedDict):
+    provider : str
+    wallet : str
+
+columns = {
+    "Game Name": "game_name",
+    "Game Code": "game_code",
+    "GameType": "type",
+}
+
+fallback_columns = {
+    "Game  Name": "game_name",
+    "Game Code": "game_code",
+    "GameType": "type",
+}
+
+
+def get_meta_info(df: pd.DataFrame, col_index: int):
+    while pd.isna(df.iloc[0][col_index]):
+        col_index -=1
+        if col_index < 0:
+            raise MetaDoesNotExistException()
+
+    return MetaInfo(
+        provider=df.iloc[1][col_index],
+        wallet=df.iloc[0][col_index],
+    )
+
+
+def process_strings(df: pd.DataFrame) -> pd.DataFrame:
+    df['type'] = df['type'].str.lower()
+    df['game_code'] = df['game_code'].str.replace('\t', '')
+    df['game_name'] = df['game_name'].str.replace('\t', '')
+    df['game_name'] = df['game_name'].str.replace('\u00a0', ' ')
+    df['game_name'] = df['game_name'].str.replace('\u200b', '')
+    df['game_name'] = df['game_name'].str.replace('\u2019S', '`')
+    df['game_name'] = df['game_name'].str.replace('\u2019s', '`')
+    df['game_name'] = df['game_name'].str.replace('\u2122', '')  # Trade mark
+    return df
+
+
+@click.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', type=click.Path(), default=Path('output.json'), show_default=True)
+def main(input: Path, output: Path):
+    result = { 'data': [] }
+
+    with open(input, 'rb') as input:
+        xl = pd.ExcelFile(input)
+        
+        for sheet in progress_bar(xl.sheet_names):
+            progress_bar.write(sheet)
+            meta_df = pd.read_excel(input, sheet, header=None, nrows=2)
+            
+            last_col_index = meta_df.columns.to_list()[-1]
+            meta_info = get_meta_info(meta_df, last_col_index)
+
+            try:
+                data_df = pd.read_excel(
+                    input, sheet, header=2, usecols=columns.keys()
+                ).rename(columns=columns)
+            except ValueError:
+                data_df = pd.read_excel(
+                    input, sheet, header=2, usecols=fallback_columns.keys()
+                ).rename(columns=fallback_columns)
+
+            data_df = data_df[data_df['game_code'].notnull()]
+            data_df = process_strings(data_df)
+
+            for data_type in data_df['type'].unique():
+                result['data'].append({
+                    'provider': meta_info['provider'],
+                    'type': data_type,
+                    'wallet': meta_info['wallet'],
+                    'games': data_df[data_df['type'] == data_type][['game_name', 'game_code']].to_dict('records')
+                })
+
+    with open(output, 'w') as output:
+        json.dump(result, output, indent=2)
+
+
+if __name__ == '__main__':
+    main()
